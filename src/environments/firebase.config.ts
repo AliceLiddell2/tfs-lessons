47d9e281b6f5ce16f1cf14e9da2dcc1e4d3cec31
AliceLiddell2 <AliceLiddell2@users.noreply.github.com>
2017-11-26 22:18:10 +0300

    upd

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,9 +1,9 @@
 /*tslint:disable*/
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyAMw11VZ7ws-aE9M_qITS9QpLU8s7jmKKk",
+  authDomain: "tink-1-proj.firebaseapp.com",
+  databaseURL: "https://tink-1-proj.firebaseio.com",
+  projectId: "tink-1-proj",
+  storageBucket: "tink-1-proj.appspot.com",
+  messagingSenderId: "45080225863"
 };
